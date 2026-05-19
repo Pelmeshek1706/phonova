3dfe1a94f08972eaffeca983785dc133d72ffc10
Pelmeshek1706 <basenko.nikita.r@gmail.com>
2026-05-19 20:07:49 +0200

    clean repo

--- openwillis/openwillis-speech/src/openwillis/speech/__init__.py
+++
@@ -1,5 +0,0 @@
-from .speech_attribute import (
-    speech_characteristics,
-)
-
-__all__ = ["speech_characteristics"]
--- openwillis/openwillis-speech/src/phonova/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Public entry points for the OpenWillis speech refinement package."""
-
-from .analyzer import SpeechAnalyzer
-from .config import SpeechAnalyzerSettings
-
-__all__ = ["SpeechAnalyzer", "SpeechAnalyzerSettings"]
